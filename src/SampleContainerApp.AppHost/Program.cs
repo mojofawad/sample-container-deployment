484ac60f700cc0854f60b89546b78a4f5f69a989
joey fawad <mojofawad@gmail.com>
2024-11-02 00:55:02 -0400

    configure backend to allow https

--- src/SampleContainerApp.AppHost/Program.cs
+++ src/SampleContainerApp.AppHost/Program.cs
@@ -4,6 +4,8 @@ var postgres = builder.AddPostgres("postgres");
 var pgdb = postgres.AddDatabase("pgdb");
 
 var api = builder.AddProject<Projects.SampleContainerApp_Api>("api")
+    .WithEndpoint(port: 5000, scheme: "http")  // Explicitly set HTTP
+    .WithEnvironment("ASPNETCORE_URLS", "http://*:8080")
     .WithReference(pgdb)
     .WithExternalHttpEndpoints();
 
