9ed37aa6302ee0f354a8c853fb9889e07c8a0fe1
joey fawad <mojofawad@gmail.com>
2024-11-02 23:15:37 -0400

    add dockerfile

--- src/SampleContainerApp.AppHost/Program.cs
+++ src/SampleContainerApp.AppHost/Program.cs
@@ -1,12 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres");
-var pgdb = postgres.AddDatabase("pgdb");
+// var postgres = builder.AddPostgres("postgres");
+// var pgdb = postgres.AddDatabase("pgdb");
 
 var api = builder.AddProject<Projects.SampleContainerApp_Api>("api")
     .WithEndpoint(port: 5000, scheme: "http")  // Explicitly set HTTP
     .WithEnvironment("ASPNETCORE_URLS", "http://*:8080")
-    .WithReference(pgdb)
+    // .WithReference(pgdb)
     .WithExternalHttpEndpoints();
 
 builder.AddNpmApp("next-app", "../SampleContainerApp.Web")
